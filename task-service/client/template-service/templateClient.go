3b64938120d550051b031a6c103a02f52c4a7bf1
weebatt <408116@niuitmo.ru>
2025-04-28 16:12:00 +0300

    feat: new feature for API whole service

--- task-service/client/template-service/templateClient.go
+++
@@ -1,7 +0,0 @@
-package templateClient
-
-import "net/http"
-
-type TemplateClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
--- worker-service/internal/models/task.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Task struct {
-	ID      int64                  `json:"id"`
-	Content map[string]interface{} `json:"content"`
-}
